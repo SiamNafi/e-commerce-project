599cce78adc4b2b2597a3fe8254f8a35db390f51
Nafiuls <snafiul700@gmail.com>
2025-07-08 18:58:43 +0600

    asdf

--- backend/server.js
+++ backend/server.js
@@ -28,10 +28,10 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
+  // app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  // });
 }
 app.listen(PORT, () => {
   console.log("server is running on http://localhost:" + PORT);
